8f80468ec77ea0b4aa1d1663c791ae6beee88d12
Danilo Casimiro <dan_casimiro@hotmail.com>
2024-01-27 21:04:46 -0300

    Add wrap in transaction in controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@
 
 class ApplicationController < ActionController::API
   include JwtToken
+
+  around_action :wrap_in_transaction
+
+  def wrap_in_transaction(&block)
+    ActiveRecord::Base.transaction(&block)
+  end
 end
