48eaa3cd8a6682035812d71b805dd3a8ae0f27ad
Danilo Casimiro <dan_casimiro@hotmail.com>
2024-02-15 07:48:58 -0300

    Refact PaginationSerialize module

--- app/controllers/concerns/pagination_serializer.rb
+++ app/controllers/concerns/pagination_serializer.rb
@@ -3,15 +3,34 @@
 module PaginationSerializer
   extend ActiveSupport::Concern
 
-  PER_PAGE=10
+  PER_PAGE = 10
 
   def paginate(collection)
-    if params[:before].present?
-      collection.where("#{collection.table_name}.id < ?", params[:before]).order(id: :desc).limit(PER_PAGE).as_json(include: include_associations)
-    elsif params[:after].present?
-      collection.where("#{collection.table_name}.id > ?", params[:after]).order(:id).limit(PER_PAGE).as_json(include: include_associations)
-    else
-      collection.order(:id).limit(PER_PAGE).as_json(include: include_associations)
-    end
+    @collection = collection
+    total_pages
+    return before if params[:before].present?
+
+    return after if params[:after].present?
+
+    collection.order(:id).limit(PER_PAGE).as_json(include: include_associations)
+  end
+
+  def before
+    @collection.where("#{@collection.table_name}.id < ?",
+                      params[:before]).order(id: :desc).limit(PER_PAGE).as_json(include: include_associations).reverse!
+  end
+
+  def after
+    return unless params[:after].present?
+
+    @collection.where("#{@collection.table_name}.id > ?",
+                      params[:after]).order(:id).limit(PER_PAGE).as_json(include: include_associations)
+  end
+
+  def total_pages
+    collection_length = @collection.count
+
+    response.headers['Total-Pages'] =
+      collection_length.positive? ? (collection_length.to_f / PER_PAGE).ceil : 0
   end
 end
